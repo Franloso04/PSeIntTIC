d2b70dd0a942230a339d1c96832bf7fc9dec4887
Franloso04 <franloso04@gmail.com>
2022-03-29 08:58:36 +0200

    1-7

---
+++ EjerciciosSecuenciales.py/Ej4.py
@@ -0,0 +1,9 @@
+n1 = int(input("Dime un numero: "))
+n2 = int(input("Dime otro numero: "))
+
+suma = n1 + n2
+resta = n1 - n2
+producto = n1 * n2
+cociente = n1 / n2
+
+print("La suma es", suma, "la resta es", resta, "el producto es", producto, "y el cociente es", cociente)
---
+++ EjerciciosSecuenciales.py/Ej6.py
@@ -0,0 +1,8 @@
+print("Introduce 3 numeros: ")
+n1 = int(input("Numero 1: "))
+n2 = int(input("Numero 2: "))
+n3 = int(input("Numero 3: "))
+
+media = (n1 + n2 + n3) / 3
+
+print("La media de los numeros es:", media)
